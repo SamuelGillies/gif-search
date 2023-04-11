c1811198874b5b125718008aa2ce946d07ad3908
Sam Gillies <sam@samgillies.com>
2023-04-11 13:52:31 +0100

    code refactored as async/await functions

--- scripts.js
+++ scripts.js
@@ -8,32 +8,20 @@ const form = document.getElementById('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     s = search.value; 
-
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${k}&s=${s}`, {
-        mode: 'cors'
-        })
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            img.src = response.data.images.original.url;
-        })
-        .catch(function(error) {
-            img.src = 'error.svg';
-        });
+    getGif();
 }); 
 
+async function getGif() {
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${k}&s=${s}`, {mode: 'cors'}); 
+        const gifData = await response.json(); 
+        img.src = gifData.data.images.original.url;
+    } catch (error) {
+        img.src = 'error.svg';
+        console.log(error); 
+    }
+}
+
 (function init() { 
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${k}&s=${s}`, {
-        mode: 'cors'
-        })
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            img.src = (response.data.images.original.url);
-        })
-        .catch(function(error) {
-            img.src = 'error.svg';
-        });
+    getGif();
 })();
